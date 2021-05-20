52b695fca62327ab8cfa4fa78aae6b71b7c64ed2
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-20 12:03:29 -0600

    Update events.mode.js

--- Gestor_de_hoteles/src/models/events.mode.js
+++ Gestor_de_hoteles/src/models/events.mode.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EventSchema = Schema ({
-    event_id: Number,
     type_event_id: { type: Schema.ObjectId, ref: 'typeEvent'} 
 });
 
