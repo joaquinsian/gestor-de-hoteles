58d8118349c731801ec9f0cbe81ece402f336a9f
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-20 12:04:51 -0600

    Update hotels.model.js

--- Gestor_de_hoteles/src/models/hotels.model.js
+++ Gestor_de_hoteles/src/models/hotels.model.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var hotelSchema = Schema({
-    hotel_id: Number,
     description: String,
     event_id: { type: Schema.ObjectId, ref: 'Event'},
     room_id: { type: Schema.ObjectId, ref: 'Room'}
