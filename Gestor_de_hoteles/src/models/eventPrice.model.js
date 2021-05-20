765e4d62353704aef90d7ceb0470d53412767779
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-20 12:02:38 -0600

    Update eventPrice.model.js

--- Gestor_de_hoteles/src/models/eventPrice.model.js
+++ Gestor_de_hoteles/src/models/eventPrice.model.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventPrice_Schema = Schema({
-    eventPrice_id: Number,
     price: String,
     type_Event_id: { type: Schema.ObjectId, ref: 'typeEvent'}
 });
