c9a8657c998beeb02b416db94923aa4681595f85
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-20 12:06:15 -0600

    Update roomPrices.model.js

--- Gestor_de_hoteles/src/models/roomPrices.model.js
+++ Gestor_de_hoteles/src/models/roomPrices.model.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var roomPrice_Schema = Schema({
-    roomPrice_id: Number,
     price: String,
     typeRoom_id: {type: Schema.ObjetId, ref:'typeRoom'},
     season_id: { type: Schema.ObjetId, ref: 'Season'}
