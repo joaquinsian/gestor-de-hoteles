ac8859d340fc0563f2a88122f31552802f447e5a
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-20 12:07:27 -0600

    Update rooms.model.js

--- Gestor_de_hoteles/src/models/rooms.model.js
+++ Gestor_de_hoteles/src/models/rooms.model.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var RoomSchema = Schema({
-    room_id: Number,
     stock: Number,
     type_room_id: { type: Schema.ObjetId, ref:'typeroom'}
 });
