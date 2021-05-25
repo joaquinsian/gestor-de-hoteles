dbbc69d281690d7e040da86f8a255e7fa516e3ae
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-25 06:20:58 -0600

    Update data.routes.js

--- Gestor_de_hoteles/src/routes/data.routes.js
+++ Gestor_de_hoteles/src/routes/data.routes.js
@@ -8,6 +8,12 @@ const typeEventD = require('../controllers/typeEventD.controller');
 const ServiceD = require('../controllers/serviceD.controller');
 const SeasonD = require('../controllers/seasonD.controller');
 const EventPriceD = require('../controllers/eventPriceD.controller');
+const EventsD = require('../controllers/eventsD.controller');
+const ExpensesD = require('../controllers/expensesD.controller');
+const HotelD = require('../controllers/hotelD.conotroller');
+const ReservationD = require('../controllers/reservationD.controller');
+const roomPriceD = require('../controllers/roomPriceD.controller');
+const RoomD = require('../controllers/roomD.controller');
 
 
 router.post('/typeRoom', typeRoomD.createTypeRoom);
@@ -15,5 +21,11 @@ router.post('/typeEvent', typeEventD.createtypeEvent);
 router.post('/Service', ServiceD.createService);
 router.post('/Season', SeasonD.createSeason); 
 router.post('/eventPrice', EventPriceD.createeventPrice);
+router.post('/Events', EventsD.createEvent);
+router.post('/Expenses', ExpensesD.createExpense);
+router.post('/Hotel', HotelD.createHotel);
+router.post('/Reservation', ReservationD.createReservation);
+router.post('/RoomPrice', RoomPriceD.createroomPrice);
+router.post('/Room', RoomD.createRoom);
 
 module.exports = router; 
