5660581bcb91f47f70b3be2ffe6caf4d287e590f
osian2019017 <61743744+joaquinsian@users.noreply.github.com>
2021-05-24 19:27:09 -0600

    Update data.routes.js

--- Gestor_de_hoteles/src/routes/data.routes.js
+++ Gestor_de_hoteles/src/routes/data.routes.js
@@ -6,13 +6,14 @@ const router = express.Router();
 const typeRoomD = require('../controllers/typeRoomD.controller');
 const typeEventD = require('../controllers/typeEventD.controller');
 const ServiceD = require('../controllers/serviceD.controller');
-const SeasonD = require('../controllers/seasonD.controller')
-
+const SeasonD = require('../controllers/seasonD.controller');
+const EventPriceD = require('../controllers/eventPriceD.controller');
 
 
 router.post('/typeRoom', typeRoomD.createTypeRoom);
 router.post('/typeEvent', typeEventD.createtypeEvent);
 router.post('/Service', ServiceD.createService);
 router.post('/Season', SeasonD.createSeason); 
+router.post('/eventPrice', EventPriceD.createeventPrice);
 
 module.exports = router; 
